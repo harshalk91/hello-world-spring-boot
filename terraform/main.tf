9785652f164abec3a1f0e017a483eff62d0b8dd8
Harshal Kulkarni <harshalk.91@gmail.com>
2022-10-30 15:32:12 +0530

    removed run.sh and modified workflows

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,4 @@ resource "dockerhub_repository" "dokerhub_repo" {
   name             = "hello-world-spring-boot"
   namespace        = "harshalk91"
   description      = "hello-world-spring-boot"
-  full_description = "Readme."
 }
-
-#resource "dockerhub_token" "master_token" {
-#  label = "master_token"
-#  scopes = ["repo:admin"]
-#}
